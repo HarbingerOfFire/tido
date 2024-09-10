df5db017b54dd279b5f1a536e33a84994c74f0af
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-09-10 15:37:14 -0600

    add requirement to be admin

--- tido.bat
+++ tido.bat
@@ -1,3 +1,13 @@
 @echo OFF
+NET SESSION
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
+GOTO ADMINTASKS
 
+:ELEVATE
+CD /d %~dp0
+set vm=%1
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '%1', '', 'runas', 1);close();"
+EXIT
+
+:ADMINTASKS
 powershell -ExecutionPolicy Bypass -File "tido.ps1" %*
