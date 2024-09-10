c74cb04ff1b1c31b9916fafe6803709dbbb1861b
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-09-10 15:41:35 -0600

    Update tido.bat

--- tido.bat
+++ tido.bat
@@ -1,4 +1,5 @@
 @echo OFF
+
 NET SESSION
 IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
 GOTO ADMINTASKS
@@ -6,8 +7,7 @@ GOTO ADMINTASKS
 :ELEVATE
 CD /d %~dp0
 set vm=%1
-MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '%1', '', 'runas', 1);close();"
-EXIT
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '%1', '', 'runas', 0);close();"
 
 :ADMINTASKS
 powershell -ExecutionPolicy Bypass -File "tido.ps1" %*
