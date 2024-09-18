a4cde34cbb42587fa0d2b7ff3b596f4c12100141
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-09-17 18:42:35 -0600

    Update tido.bat

--- tido.bat
+++ tido.bat
@@ -9,7 +9,4 @@ set vm=%1
 MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '%1', '', 'runas', 1);close();"
 
 :ADMINTASKS
-set vm=%1
-@echo vm is %vm%
-powershell Set-VMProcessor -VMName "%vm%" -ExposeVirtualizationExtensions $true
-PAUSE
+powershell -ExecutionPolicy Bypass -File "tido.ps1" %*
